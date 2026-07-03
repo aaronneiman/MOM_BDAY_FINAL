c97958344b4152d15c86ed98cfece1ac1f7fcc6b
Aaron Neiman <aaronneiman@proton.me>
2026-07-03 16:05:52 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+#include <TFT_eSPI.h>
+#include "secrets.h"
+
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
